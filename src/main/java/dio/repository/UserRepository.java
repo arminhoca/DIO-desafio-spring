5602d2d4284b98d6f799f9e70c3d86a0861b55e3
yasmin <yasminkennyasa@gmail.com>
2024-07-28 11:40:06 -0300

    commit

---
+++ src/main/java/dio/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package dio.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import dio.model.User;
+
+public interface UserRepository extends JpaRepository< User, Integer>{
+
+   
+}
+
+
+    
